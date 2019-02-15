ab6bcde2820d125c65f8ba42eb5d026aa6409c44
Laksh Bhambhani <lakshbh@gmail.com>
2019-02-15 01:43:27 -0800

    Add files via upload

---
+++ ArduinoQuadruped.ino
@@ -0,0 +1,70 @@
+#include <Servo.h>
+
+Servo FL_HIP; 
+Servo FL_FOOT;
+Servo FR_HIP;
+Servo FR_FOOT;
+Servo BL_HIP; 
+Servo BL_FOOT;
+Servo BR_HIP;
+Servo BR_FOOT;
+
+
+void setup() { 
+  FL_HIP.attach(4); 
+  FL_FOOT.attach(5);
+  FR_HIP.attach(6);
+  FR_FOOT.attach(7);
+  BL_HIP.attach(8);
+  BL_FOOT.attach(9);
+  BR_HIP.attach(10);
+  BR_FOOT.attach(11);
+
+  FL_HIP.write(40);
+  FL_FOOT.write(110);
+  FR_HIP.write(130);
+  FR_FOOT.write(70);
+  BL_HIP.write(130);
+  BL_FOOT.write(70);
+  BR_HIP.write(40);
+  BR_FOOT.write(110);
+}
+
+void loop() {
+ leanLeft();
+ delay(1000);
+ homePos();
+ delay(1000);
+ leanRight();
+ delay(1000);
+ homePos();
+ delay(1000);
+} 
+
+void leanRight()
+{
+ FL_FOOT.write(15);
+ FR_FOOT.write(15);
+ BL_FOOT.write(140);
+ BR_FOOT.write(140);
+}
+
+void leanLeft()
+{
+ FL_FOOT.write(140);
+ FR_FOOT.write(140);
+ BL_FOOT.write(15);
+ BR_FOOT.write(15);
+}
+
+void homePos()
+{
+ FL_HIP.write(40);
+ FL_FOOT.write(110);
+ FR_HIP.write(130);
+ FR_FOOT.write(70);
+ BL_HIP.write(130);
+ BL_FOOT.write(70);
+ BR_HIP.write(40);
+ BR_FOOT.write(110);
+}
